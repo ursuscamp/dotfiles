721cffa0184cc7da58fbce05d0c86761b533b4d6
ursuscamp <ryan@breen.xyz>
2024-02-23 15:52:56 -0500

    Getting some stuff working in neovim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -3,14 +3,14 @@ vim.g.maplocalleader = ' '
 
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  }
+	vim.fn.system {
+		'git',
+		'clone',
+		'--filter=blob:none',
+		'https://github.com/folke/lazy.nvim.git',
+		'--branch=stable', -- latest stable release
+		lazypath,
+	}
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -19,90 +19,92 @@ vim.opt.rtp:prepend(lazypath)
 -------------------
 
 require('lazy').setup({
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  { 'folke/which-key.nvim', opts = {} },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    }
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function () 
-      local configs = require("nvim-treesitter.configs")
+	{ "catppuccin/nvim",      name = "catppuccin", priority = 1000 },
+	{ 'folke/which-key.nvim', opts = {} },
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		}
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "javascript", "html", "rust" },
-          sync_install = false,
-          auto_install = true,
-          highlight = { enable = true },
-          indent = { enable = true },  
-        })
-    end
-  },
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-     dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-  {'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'},
-  {
-    'numToStr/Comment.nvim',
-    opts = {},
-    lazy = false,
-  },
+			configs.setup({
+				ensure_installed = { "c", "lua", "vim", "vimdoc", "javascript", "html", "rust" },
+				sync_install = false,
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end
+	},
+	{
+		'nvim-telescope/telescope.nvim',
+		tag = '0.1.5',
+		dependencies = { 'nvim-lua/plenary.nvim' }
+	},
+	{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+	{ 'akinsho/bufferline.nvim',                  version = "*", dependencies = 'nvim-tree/nvim-web-devicons' },
+	{
+		'numToStr/Comment.nvim',
+		opts = {},
+		lazy = false,
+	},
 
-  {'williamboman/mason.nvim'},
-  {'williamboman/mason-lspconfig.nvim'},
-  {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v3.x',
-    lazy = true,
-    config = false,
-  },
-  {
-    'neovim/nvim-lspconfig',
-    dependencies = {
-      {'hrsh7th/cmp-nvim-lsp'},
-    }
-  },
-  {
-    'hrsh7th/nvim-cmp',
-    dependencies = {
-      {'L3MON4D3/LuaSnip'}
-    },
-  },
-  {'lvimuser/lsp-inlayhints.nvim', opts = {} },
-  -- {
-  --   "folke/trouble.nvim",
-  --   dependencies = { "nvim-tree/nvim-web-devicons" },
-  --   opts = {
-  --    -- your configuration comes here
-  --    -- or leave it empty to use the default settings
-  --    -- refer to the configuration section below
-  --   },
-  -- },
-  { 'stevearc/conform.nvim'  },
-  { 'j-hui/fidget.nvim', opts = {} },
-  {
-    -- Set lualine as statusline
-    'nvim-lualine/lualine.nvim',
-    -- See `:help lualine.txt`
-    opts = {
-      options = {
-        icons_enabled = true,
-        theme = 'auto',
-        component_separators = '|',
-        section_separators = '',
-      },
-    },
-  },
+	{ 'williamboman/mason.nvim' },
+	{ 'williamboman/mason-lspconfig.nvim' },
+	{
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v3.x',
+		lazy = true,
+		config = false,
+	},
+	{
+		'neovim/nvim-lspconfig',
+		dependencies = {
+			{ 'hrsh7th/cmp-nvim-lsp' },
+		}
+	},
+	{
+		'hrsh7th/nvim-cmp',
+		dependencies = {
+			{ 'L3MON4D3/LuaSnip' }
+		},
+	},
+	{ 'lvimuser/lsp-inlayhints.nvim', opts = {} },
+	-- {
+	--   "folke/trouble.nvim",
+	--   dependencies = { "nvim-tree/nvim-web-devicons" },
+	--   opts = {
+	--    -- your configuration comes here
+	--    -- or leave it empty to use the default settings
+	--    -- refer to the configuration section below
+	--   },
+	-- },
+	{ 'stevearc/conform.nvim' },
+	{ 'j-hui/fidget.nvim',            opts = {} },
+	{
+		-- Set lualine as statusline
+		'nvim-lualine/lualine.nvim',
+		-- See `:help lualine.txt`
+		opts = {
+			options = {
+				icons_enabled = true,
+				theme = 'auto',
+				component_separators = '|',
+				section_separators = '',
+			},
+		},
+	},
+	{ "folke/neodev.nvim" },
 })
 
 -------------------
@@ -132,57 +134,59 @@ vim.opt.termguicolors = true
 -------------------
 
 require('telescope').setup({
-  defaults = {
-    mappings = {
-      i = {
-        ["<C-j>"] = "move_selection_previous",
-        ["<C-k>"] = "move_selection_next",
-        ["<C-h>"] = "which_key",
-      }
-    },
-    layout_config = {
-      prompt_position = "top",
-    },
-    sorting_strategy = "ascending",
-  }
+	defaults = {
+		mappings = {
+			i = {
+				["<C-j>"] = "move_selection_previous",
+				["<C-k>"] = "move_selection_next",
+				["<C-h>"] = "which_key",
+			}
+		},
+		layout_config = {
+			prompt_position = "top",
+		},
+		sorting_strategy = "ascending",
+	}
 })
 require('telescope').load_extension('fzf')
 
 require('bufferline').setup({
-  options = {
-    diagnostics = "nvim_lsp",
-    offsets = {
-      { filetype = "neo-tree" },
-    },
-  }
+	options = {
+		diagnostics = "nvim_lsp",
+		offsets = {
+			{ filetype = "neo-tree" },
+		},
+	}
 })
 
 
 require('conform').setup({
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 500,
+		lsp_fallback = true,
+	},
 })
 
 
 -- LSP stuff
+require('neodev').setup()
+
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
-  require('lsp-inlayhints').on_attach(client, bufnr)
+	-- see :help lsp-zero-keybindings
+	-- to learn the available actions
+	lsp_zero.default_keymaps({ buffer = bufnr })
+	require('lsp-inlayhints').on_attach(client, bufnr)
 end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = { "rust_analyzer", "lua_ls", "tsserver", "html" },
-  handlers = {
-    lsp_zero.default_setup,
-  },
+	ensure_installed = { "rust_analyzer", "lua_ls", "tsserver", "html" },
+	handlers = {
+		lsp_zero.default_setup,
+	},
 })
 
 vim.diagnostic.config()
@@ -191,12 +195,12 @@ vim.diagnostic.config()
 ---- Keymaps
 -------------------
 vim.keymap.set('n', '<leader>w', ':w<CR>', { desc = "Write file" })
-vim.keymap.set('n', '<C-q>', ':qa<CR>', { desc = "Quit" } )
+vim.keymap.set('n', '<C-q>', ':qa<CR>', { desc = "Quit" })
 vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = "Move left window" })
 vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = "Move down window" })
 vim.keymap.set('n', '<C-k>', '<C-k>h', { desc = "Move up window" })
 vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = "Move right window" })
-vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>', { desc = "N[e]otree" } )
+vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>', { desc = "N[e]otree" })
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find files" })
@@ -215,8 +219,8 @@ vim.keymap.set('n', '<leader>lS', builtin.lsp_workspace_symbols, { desc = "Works
 vim.keymap.set('n', '<leader>ld', ':Telescope diagnostics bufnr=0<CR>', { desc = "Buffer diagnostics" })
 vim.keymap.set('n', '<leader>lD', builtin.diagnostics, { desc = "Workspace diagnostics" })
 
-vim.keymap.set('n', ']b', ':BufferLineCycleNext<CR>', { desc =  "Next buffer" })
-vim.keymap.set('n', '[b', ':BufferLineCyclePrev<CR>', { desc =  "Previous buffer" })
+vim.keymap.set('n', ']b', ':BufferLineCycleNext<CR>', { desc = "Next buffer" })
+vim.keymap.set('n', '[b', ':BufferLineCyclePrev<CR>', { desc = "Previous buffer" })
 vim.keymap.set('n', ']B', ':BufferLineMoveNext<CR>', { desc = "Move buffer to the right" })
 vim.keymap.set('n', '[B', ':BufferLineMovePrev<CR>', { desc = "Move buffer to the left" })
 
@@ -224,13 +228,13 @@ local cmp = require('cmp')
 local cmp_action = require('lsp-zero').cmp_action()
 
 cmp.setup({
-  mapping = cmp.mapping.preset.insert({
-    ['<Tab>'] = cmp_action.tab_complete(),
-    ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
-    ['<CR>'] = cmp.mapping.confirm({select = false}),
-  }),
-  window = {
-    completion = cmp.config.window.bordered(),
-    documentation = cmp.config.window.bordered(),
-  },
+	mapping = cmp.mapping.preset.insert({
+		['<Tab>'] = cmp_action.luasnip_supertab(),
+		['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+		['<CR>'] = cmp.mapping.confirm({ select = false }),
+	}),
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
 })
