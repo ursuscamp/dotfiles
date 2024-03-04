53c8dad5c9b7bf38dee363bd124b96e885abec7d
Ryan Breen <ryan@breen.xyz>
2024-03-04 09:17:09 -0500

    removing regtest alias for now

--- config/fish/functions/regtest.fish
+++
@@ -1,3 +0,0 @@
-function regtest --wraps='bitcoin-cli -regtest -rpcuser=regtest -rpcpassword=regtest' --description 'alias regtest bitcoin-cli -regtest -rpcuser=regtest -rpcpassword=regtest'
-  bitcoin-cli -regtest -rpcuser=regtest -rpcpassword=regtest $argv; 
-end
