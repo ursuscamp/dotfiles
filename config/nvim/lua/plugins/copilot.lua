c71d31bd5daed498b84d54155d8827be4241c158
Ryan Breen <ryan@breen.xyz>
2024-05-16 16:06:44 -0400

    More Copilot tweaks

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -1,7 +1,27 @@
 -- Load this plugin only on my work machine
-if vim.fn.getenv('USER') ~= 'rbreen' then
-	return {
-		'github/copilot.vim',
-		cmd = 'Copilot',
-	}
+local config = {
+	"CopilotC-Nvim/CopilotChat.nvim",
+	branch = "canary",
+	dependencies = {
+		{ "github/copilot.vim" },
+		{ "nvim-lua/plenary.nvim" },
+	},
+	config = function()
+		require("CopilotChat").setup()
+		vim.cmd(":Copilot enable")
+
+		vim.keymap.set({ 'n', 'v' }, '<leader>gcc', '<cmd>CopilotChatToggle<CR>',
+			{ desc = "Toggle Copilot Chat" })
+		vim.keymap.set({ 'n', 'v' }, '<leader>gce', '<cmd>Copilot enable<CR>',
+			{ desc = "Enable Copilot suggestions" })
+		vim.keymap.set({ 'n', 'v' }, '<leader>gcd', '<cmd>Copilot disable<CR>',
+			{ desc = "Disable Copilot suggestions" })
+	end,
+	lazy = true
+}
+
+if vim.fn.getenv('USER') == 'rbreen' then
+	config.event = 'VeryLazy'
 end
+
+return config
