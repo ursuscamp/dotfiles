f576e7dd06ee66369954d75f53746b3f51be5f43
Ryan Breen <ryan@breen.xyz>
2024-06-04 22:33:43 -0400

    Deconflicting flash.nvim and nvim-surround 'S' key

--- config/nvim/lua/plugins/flash.lua
+++ config/nvim/lua/plugins/flash.lua
@@ -5,10 +5,10 @@ end
 return {
 	'folke/flash.nvim',
 	keys = {
-		{ "s",     mode = { "n", "x", "o" }, function() f().jump() end,              desc = "Flash" },
-		{ "S",     mode = { "n", "x", "o" }, function() f().treesitter() end,        desc = "Flash Treesitter" },
-		{ "r",     mode = "o",               function() f().remote() end,            desc = "Remote Flash" },
-		{ "R",     mode = { "o", "x" },      function() f().treesitter_search() end, desc = "Treesitter Search" },
-		{ "<c-s>", mode = { "c" },           function() f().toggle() end,            desc = "Toggle Flash Search" },
+		{ "s",         mode = { "n", "x", "o" }, function() f().jump() end,              desc = "Flash" },
+		{ "<leader>S", mode = { "n", "x", "o" }, function() f().treesitter() end,        desc = "Flash Treesitter" },
+		{ "r",         mode = "o",               function() f().remote() end,            desc = "Remote Flash" },
+		{ "R",         mode = { "o", "x" },      function() f().treesitter_search() end, desc = "Treesitter Search" },
+		{ "<c-s>",     mode = { "c" },           function() f().toggle() end,            desc = "Toggle Flash Search" },
 	},
 }
