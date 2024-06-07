174f677f7539cb81a9e35eb0c4d2f5df613dacc4
Ryan Breen <ryan@breen.xyz>
2024-06-07 09:10:50 -0400

    Gitsigns hunk keymaps

--- config/nvim/lua/plugins/gitsigns.lua
+++ config/nvim/lua/plugins/gitsigns.lua
@@ -3,5 +3,7 @@ return {
 	event = { "BufReadPost", "BufNewFile" },
 	config = function()
 		require('gitsigns').setup()
+		vim.keymap.set('n', '[h', function() require('gitsigns').prev_hunk() end, { desc = 'Previous hunk' })
+		vim.keymap.set('n', ']h', function() require('gitsigns').next_hunk() end, { desc = 'Next hunk' })
 	end
 }
