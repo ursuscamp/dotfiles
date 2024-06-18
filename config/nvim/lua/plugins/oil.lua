1e4bab872b951cb7abb0fa73c75752a539c57d55
Ryan Breen <ryan@breen.xyz>
2024-06-18 13:31:13 -0400

    oil.nvim: show hidden files

--- config/nvim/lua/plugins/oil.lua
+++ config/nvim/lua/plugins/oil.lua
@@ -4,5 +4,14 @@ return {
 	keys = {
 		{ '<leader>e', '<cmd>Oil --float<CR>', desc = "Open file explorer" },
 	},
-	opts = {},
+	opts = {
+		default_file_explorer = true,
+		skip_confirm_for_simple_edits = true,
+		view_options = {
+			show_hidden = true,
+			is_always_hidden = function(name, _)
+				return name == ".." or name == ".git"
+			end,
+		}
+	},
 }
