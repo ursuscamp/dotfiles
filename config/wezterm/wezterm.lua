d53f35a9ad46bddc6f69b4b6d412663af371a2bd
Ryan Breen <ryan@breen.xyz>
2024-06-20 10:41:54 -0400

    Format tab title with zoomed pane

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -110,4 +110,33 @@ smart_splits.apply_to_config(config, {
 	},
 })
 
+-- This function returns the suggested title for a tab.
+-- It prefers the title that was set via `tab:set_title()`
+-- or `wezterm cli set-tab-title`, but falls back to the
+-- title of the active pane in that tab.
+local function tab_title(tab_info)
+	local title = tab_info.tab_title
+	-- if the tab title is explicitly set, take that
+	if title and #title > 0 then
+		return title
+	end
+	-- Otherwise, use the title from the active pane
+	-- in that tab
+	return tab_info.active_pane.title
+end
+
+function on_format_tab_title(tab, _tabs, _panes, _config, _hover, _max_width)
+	local zoomed = ''
+	local index = tab.tab_index + 1
+	local title = tab_title(tab)
+	if tab.active_pane.is_zoomed then
+		zoomed = '   🔍'
+	end
+	return {
+		{ Text = string.format(' %d: %s%s ', index, title, zoomed) }
+	}
+end
+
+wezterm.on('format-tab-title', on_format_tab_title)
+
 return config
