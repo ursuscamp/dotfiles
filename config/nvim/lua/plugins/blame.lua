5e10a0ca44a6ab0755efe145bd4a680266e32ad6
Ryan Breen <ryan@breen.xyz>
2024-06-25 14:18:44 -0400

    Fixing git blame keybind

--- config/nvim/lua/plugins/blame.lua
+++ config/nvim/lua/plugins/blame.lua
@@ -2,8 +2,8 @@ return {
 	"FabijanZulj/blame.nvim",
 	cmd = 'ToggleBlame',
 	keys = {
-		{ '<leader>gb', '<cmd>ToggleBlame window<CR>',  desc = "Toggle Git blame (window)" },
-		{ '<leader>gB', '<cmd>ToggleBlame virtual<CR>', desc = "Toggle Git blame (virtual)" },
+		{ '<leader>gb', '<cmd>BlameToggle window<CR>',  desc = "Toggle Git blame (window)" },
+		{ '<leader>gB', '<cmd>BlameToggle virtual<CR>', desc = "Toggle Git blame (virtual)" },
 	},
 	opts = {},
 }
