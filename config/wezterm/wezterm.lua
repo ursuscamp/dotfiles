f39d0132a1b44ad4b0e5452ef60a0e8a2763de8d
Ryan Breen <ryan@breen.xyz>
2024-07-03 11:25:22 -0400

    Wezterm open scrollback in neovim

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -64,6 +64,11 @@ config.keys = {
 			end),
 		},
 	},
+	{
+		key = 'O',
+		mods = 'CTRL|SHIFT',
+		action = wezterm.action.EmitEvent 'trigger-vim-with-scrollback',
+	}
 }
 
 -- Tabs
@@ -101,6 +106,11 @@ config.quick_select_patterns = {
 config.set_environment_variables = {
 	PATH = "/opt/homebrew/bin:/usr/local/bin:" .. os.getenv("PATH"),
 }
+
+config.command_palette_bg_color = "#282a36"
+config.command_palette_fg_color = "#bd93f9"
+config.switch_to_last_active_tab_when_closing_tab = true
+config.use_resize_increments = true
 
 smart_splits.apply_to_config(config, {
 	default_amount = 5,
@@ -144,5 +154,39 @@ function on_format_tab_title(tab, _tabs, _panes, _config, _hover, _max_width)
 end
 
 wezterm.on('format-tab-title', on_format_tab_title)
+
+
+wezterm.on('trigger-vim-with-scrollback', function(window, pane)
+	local io = require 'io'
+	local os = require 'os'
+	local act = wezterm.action
+
+	-- Retrieve the text from the pane
+	local text = pane:get_lines_as_text(pane:get_dimensions().scrollback_rows)
+
+	-- Create a temporary file to pass to vim
+	local name = os.tmpname()
+	local f = io.open(name, 'w+')
+	f:write(text)
+	f:flush()
+	f:close()
+
+	-- Open a new window running vim and tell it to open the file
+	window:perform_action(
+		act.SpawnCommandInNewTab {
+			args = { 'nvim', name },
+		},
+		pane
+	)
+
+	-- Wait "enough" time for vim to read the file before we remove it.
+	-- The window creation and process spawn are asynchronous wrt. running
+	-- this script and are not awaitable, so we just pick a number.
+	--
+	-- Note: We don't strictly need to remove this file, but it is nice
+	-- to avoid cluttering up the temporary directory.
+	wezterm.sleep_ms(1000)
+	os.remove(name)
+end)
 
 return config
