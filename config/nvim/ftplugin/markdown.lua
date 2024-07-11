7b222c4c74222236254cd4bccf214ad9a30c0371
Ryan Breen <ryan@breen.xyz>
2024-07-11 16:38:42 -0400

    markdown ftplugin to create a checkbox

---
+++ config/nvim/ftplugin/markdown.lua
@@ -0,0 +1,24 @@
+local function checkbox()
+	local line = vim.api.nvim_get_current_line()
+	-- if the line starts with '- ' then replace it with '- [ ]'
+	if string.match(line, '- 󰄱') then
+		line = string.gsub(line, '- 󰄱', '- ')
+	elseif string.match(line, '- ') then
+		line = string.gsub(line, '- ', '- 󰄱')
+	elseif string.match(line, '^%s*- ') then
+		line = string.gsub(line, '^(%s*)- ', '%1- 󰄱 ')
+	end
+	vim.api.nvim_set_current_line(line)
+end
+
+local status, wk = pcall(require, 'which-key')
+if (not status) then
+	vim.keymap.set('n', '<leader>zc', checkbox, { desc = 'Mark/create checkbox', buffer = true })
+else
+	wk.register({
+		['<leader>z'] = {
+			name = 'Buffer/filetype-specific',
+			c = { checkbox, 'Mark/create checkbox' },
+		},
+	})
+end
