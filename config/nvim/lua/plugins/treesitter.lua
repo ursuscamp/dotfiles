9c261c4c27a463ee985da0280c664eda29bc0214
Ryan Breen <ryan@breen.xyz>
2024-08-09 11:38:46 -0400

    neovim: adding diff to treesitter

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,10 +1,24 @@
 return {
-	'nvim-treesitter/nvim-treesitter',
-	event = { 'BufReadPost', 'BufNewFile' },
-	build = ':TSUpdate',
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPost", "BufNewFile" },
+	build = ":TSUpdate",
 	config = function()
-		require('nvim-treesitter.configs').setup({
-			ensure_installed = { 'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'rust', 'html', 'javascript', 'typescript' },
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"bash",
+				"c",
+				"html",
+				"lua",
+				"luadoc",
+				"markdown",
+				"vim",
+				"vimdoc",
+				"rust",
+				"html",
+				"javascript",
+				"typescript",
+				"diff",
+			},
 			auto_install = true,
 			highlight = {
 				enable = true,
