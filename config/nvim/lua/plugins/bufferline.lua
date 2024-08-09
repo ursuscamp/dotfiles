bc4465a4a01d9c77c96d8fa031b8e04bcc16d292
Ryan Breen <ryan@breen.xyz>
2024-08-09 11:39:00 -0400

    neovim: fixing bufferline colors

--- config/nvim/lua/plugins/bufferline.lua
+++ config/nvim/lua/plugins/bufferline.lua
@@ -1,40 +1,32 @@
 return {
-	'akinsho/bufferline.nvim',
+	"akinsho/bufferline.nvim",
 	version = "*",
 	event = { "BufReadPost", "BufNewFile" },
 	dependencies = {
-		'nvim-tree/nvim-web-devicons',
+		"nvim-tree/nvim-web-devicons",
 	},
 	config = function()
-		require('bufferline').setup({
+		require("bufferline").setup({
 			options = {
-				separator_style = { ' ', '' },
+				separator_style = { " ", "" },
 				-- separator_style = 'slant',
 			},
 			highlights = {
-				buffer_selected = {
-					fg = require('dracula.palette').base04,
-					bg = require('dracula.palette').purple,
-				},
-				close_button_selected = {
-					fg = require('dracula.palette').base04,
-					bg = require('dracula.palette').purple,
-				},
 				background = {
-					fg = require('dracula.palette').purple,
-					bg = require('dracula.palette').base03
+					fg = require("dracula.palette").purple,
+					bg = require("dracula.palette").base03,
 				},
 				close_button = {
-					fg = require('dracula.palette').purple,
-					bg = require('dracula.palette').base03,
+					fg = require("dracula.palette").purple,
+					bg = require("dracula.palette").base03,
 				},
 			},
 		})
 
-		vim.keymap.set('n', '<C-n>', '<cmd>BufferLineCycleNext<CR>', { desc = "Next buffer" })
-		vim.keymap.set('n', '<C-p>', '<cmd>BufferLineCyclePrev<CR>', { desc = "Previous buffer" })
-		vim.keymap.set('n', '<b', '<cmd>BufferLineMovePrev<CR>', { desc = "Move buffer back" })
-		vim.keymap.set('n', '>b', '<cmd>BufferLineMoveNext<CR>', { desc = "Move buffer forward" })
-		vim.keymap.set('n', '<leader>C', '<cmd>BufferLineCloseOthers<CR>', { desc = "Close other buffers" })
-	end
+		vim.keymap.set("n", "<C-n>", "<cmd>BufferLineCycleNext<CR>", { desc = "Next buffer" })
+		vim.keymap.set("n", "<C-p>", "<cmd>BufferLineCyclePrev<CR>", { desc = "Previous buffer" })
+		vim.keymap.set("n", "<b", "<cmd>BufferLineMovePrev<CR>", { desc = "Move buffer back" })
+		vim.keymap.set("n", ">b", "<cmd>BufferLineMoveNext<CR>", { desc = "Move buffer forward" })
+		vim.keymap.set("n", "<leader>C", "<cmd>BufferLineCloseOthers<CR>", { desc = "Close other buffers" })
+	end,
 }
