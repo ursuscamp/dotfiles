a5e53d8797dec2bac59d328b11f143b4f84af1a1
Ryan Breen <ryan@breen.xyz>
2024-08-16 16:27:19 -0400

    aerospace: added for experimentation

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PACKAGES="helix wezterm alacritty kitty zellij xplr fish nvim tmux zsh"
+PACKAGES="helix wezterm alacritty kitty zellij xplr fish nvim tmux zsh aerospace"
 
 REPO=~/Dev/dotfiles
 DESTINATION=$HOME
