fc4b8e8ddcddc689b06f5bd01c3d3999db993a17
Ryan Breen <ryan@breen.xyz>
2024-12-24 10:30:22 -0500

    lazyvim: customizing fzf-lua

---
+++ config/lazyvim/lua/plugins/fzf-lua.lua
@@ -0,0 +1,12 @@
+return {
+  "ibhagwan/fzf-lua",
+  opts = {
+    keymap = {
+      fzf = {
+        ["ctrl-u"] = "clear-query",
+        ["alt-u"] = "half-page-up",
+        ["alt-d"] = "half-page-down",
+      },
+    },
+  },
+}
