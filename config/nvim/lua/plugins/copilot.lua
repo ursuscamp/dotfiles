02723ec63715e6766bb6b9989cd9bdb0b9865940
Ryan Breen <ryan@breen.xyz>
2025-01-30 12:17:59 -0500

    lazyvim: copilot mappings

---
+++ config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    opts = {
+      mappings = {
+        reset = {
+          normal = "<C-r>",
+          insert = "<C-r>",
+        },
+      },
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+}
