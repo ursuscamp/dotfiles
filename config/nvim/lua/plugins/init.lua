a2eda8579635b1312f670fe367dfccc19ad95d90
Ryan Breen <ryan@breen.xyz>
2025-02-10 08:40:06 -0500

    neovim: removing seemingly unnecessary plugins

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -89,10 +89,6 @@ return {
 		opts = {},
 	},
 	{
-		"j-hui/fidget.nvim",
-		opts = {},
-	},
-	{
 		"akinsho/bufferline.nvim",
 		event = "VeryLazy",
 		dependencies = "echasnovski/mini.nvim",
@@ -117,17 +113,7 @@ return {
 	{
 		"folke/noice.nvim",
 		event = "VeryLazy",
-		opts = {
-			-- add any options here
-		},
-		dependencies = {
-			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-			"MunifTanjim/nui.nvim",
-			-- OPTIONAL:
-			--   `nvim-notify` is only needed, if you want to use the notification view.
-			--   If not available, we use `mini` as the fallback
-			"rcarriga/nvim-notify",
-		},
+		opts = {},
 	},
 	{
 		"mrjones2014/smart-splits.nvim",
