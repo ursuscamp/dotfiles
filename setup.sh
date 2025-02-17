8ea327ea4bf0e1632a413f8864850d32577b461b
Ryan Breen <ryan@breen.xyz>
2025-02-17 13:57:13 -0500

    ghostty

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PACKAGES="helix wezterm alacritty kitty zellij xplr fish nvim tmux zsh aerospace"
+PACKAGES="helix wezterm alacritty kitty zellij xplr fish nvim tmux zsh aerospace ghostty"
 
 REPO=~/Dev/dotfiles
 DESTINATION=$HOME
