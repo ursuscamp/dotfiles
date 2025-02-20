af54c9f5d41a36cfea6a2f88118d41c6bd82d17d
Ryan Breen <ryan@breen.xyz>
2025-02-19 21:53:37 -0500

    neovim: a few quick changes

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -102,7 +102,9 @@ return {
 	{
 		"MeanderingProgrammer/render-markdown.nvim",
 		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
-		opts = {},
+		opts = {
+			filetypes = { "markdown", "copilot-chat" },
+		},
 	},
 	{
 		"akinsho/bufferline.nvim",
