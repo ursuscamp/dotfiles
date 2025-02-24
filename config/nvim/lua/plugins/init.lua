97c6d349498011ed2b435e6b1af9586fd9417e68
Ryan Breen <ryan@breen.xyz>
2025-02-24 11:06:02 -0500

    neovim: markdown filetypes

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -103,8 +103,9 @@ return {
 		"MeanderingProgrammer/render-markdown.nvim",
 		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
 		opts = {
-			filetypes = { "markdown", "copilot-chat" },
+			file_types = { "markdown", "copilot-chat", "codecompanion", "Avante" },
 		},
+		ft = { "markdown", "copilot-chat", "codecompanion", "Avante" },
 	},
 	{
 		"akinsho/bufferline.nvim",
