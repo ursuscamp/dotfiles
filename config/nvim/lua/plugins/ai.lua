65fa4fe312cab2817b092e04f5eee30fec516180
Ryan Breen <ryan@breen.xyz>
2025-02-27 21:02:46 -0500

    neovim: codecompanion

--- config/nvim/lua/plugins/ai.lua
+++ config/nvim/lua/plugins/ai.lua
@@ -19,20 +19,63 @@ return {
 	{
 		"giuxtaposition/blink-cmp-copilot"
 	},
+	-- {
+	-- 	"CopilotC-Nvim/CopilotChat.nvim",
+	-- 	dependencies = {
+	-- 		{ "zbirenbaum/copilot.lua" },
+	-- 		{ "nvim-lua/plenary.nvim", branch = "master" },
+	-- 	},
+	-- 	build = "make tiktoken",
+	-- 	opts = {
+	-- 		model = copilot_model(),
+	-- 	},
+	-- 	cmd = { "CopilotChat", "CopilotChatToggle", "CopilotChatModels" },
+	-- 	keys = {
+	-- 		{ "<leader>ac", mode = { "n", "x", "o" }, vim.cmd.CopilotChatToggle, desc = "Toggle Copilot Chat" },
+	-- 		{ "<leader>am", mode = { "n", "x", "o" }, vim.cmd.CopilotChatModels, desc = "Pick chat model" },
+	-- 	},
+	-- },
 	{
-		"CopilotC-Nvim/CopilotChat.nvim",
-		dependencies = {
-			{ "zbirenbaum/copilot.lua" },
-			{ "nvim-lua/plenary.nvim", branch = "master" },
-		},
-		build = "make tiktoken",
+		"olimorris/codecompanion.nvim",
+		-- config = true,
 		opts = {
-			model = copilot_model(),
+			strategies = {
+				chat = {
+					adapter = "copilot",
+				},
+				inline = {
+					adapter = "copilot",
+				}
+			}
 		},
-		cmd = { "CopilotChat", "CopilotChatToggle", "CopilotChatModels" },
+		config = function(opts)
+			require("codecompanion").setup({
+				adapters = {
+					copilot = function()
+						return require("codecompanion.adapters").extend("copilot", {
+							schema = {
+								model = {
+									default = copilot_model(),
+								},
+							},
+						})
+					end,
+				},
+			})
+		end,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
 		keys = {
-			{ "<leader>ac", mode = { "n", "x", "o" }, vim.cmd.CopilotChatToggle, desc = "Toggle Copilot Chat" },
-			{ "<leader>am", mode = { "n", "x", "o" }, vim.cmd.CopilotChatModels, desc = "Pick chat model" },
-		},
+			{
+				"<leader>ac",
+				mode = { "n", "x", "o", },
+				function()
+					require("codecompanion").toggle()
+				end,
+				desc = "Toggle CodeCompanion chat"
+			}
+		}
 	},
 }
