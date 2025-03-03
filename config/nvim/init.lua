a586b6a6cb5ccf297461ac2c7da148c40a7e5255
Ryan Breen <ryan@breen.xyz>
2025-03-03 08:16:02 -0500

    neovim: new keybinds and plugin updates

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -29,8 +29,11 @@ vim.keymap.set("n", "<leader>|", vim.cmd.vsplit, { desc = "Vertical split" })
 vim.keymap.set("n", "<leader>\\", vim.cmd.split, { desc = "Horizontal split" })
 vim.keymap.set("n", "<leader>q", vim.cmd.quit, { desc = "Quit window" })
 vim.keymap.set("n", "<leader>L", vim.cmd.Lazy, { desc = "Lazy" })
+vim.keymap.set("n", "<leader>w", vim.cmd.write, { desc = "Write" })
+vim.keymap.set("n", "<leader>W", vim.cmd.wa, { desc = "Write all" })
 
 
+vim.keymap.set("n", 'gt', vim.lsp.buf.type_definition, { desc = 'Goto type definition' })
 vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code actions" })
 vim.keymap.set({ "n", "v" }, "<leader>cr", vim.lsp.buf.rename, { desc = "Rename under cursor" })
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
